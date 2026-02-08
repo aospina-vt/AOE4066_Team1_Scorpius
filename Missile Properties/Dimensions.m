3dcf97e5c35071df16aa5be7f5d0b469c9bf8cf6
Andres <aospina@vt.edu>
2026-02-08 18:39:46 -0500

    fixed

---
+++ Missile Properties/Dimensions.m
@@ -0,0 +1,10 @@
+function [Length, Width, Height] = Dimensions()
+
+Length = 132;
+Width = 11.89;
+Height = 9.25;
+
+end
+
+
+
---
+++ Missile Properties/Weights.m
@@ -0,0 +1,7 @@
+function [Initial, Final] = Weights()
+
+    %LBS
+    Final = 543.92;
+    Initial = 1293.92;
+
+end
