abe6df874966de8acfada8cc51194e34e7c4a1f9
vnick21 <vnick21@vt.edu>
2026-05-03 14:30:32 -0400

    testing

---
+++ Tests/structures_test_final.m
@@ -0,0 +1,21 @@
+function [stress,heating]=structures_test_final(stainless_steel,titanium_grade5,inconel718,alon)
+
+if titanium_grade5
+    stress=true;
+    heating=true;
+elseif stainless_steel
+    stress=true;
+    heating=true;
+elseif inconel718
+    stress=true;
+    heating=true;
+elseif alon
+    stress=true;
+    heating=true;
+end
+
+
+
+
+
+end
